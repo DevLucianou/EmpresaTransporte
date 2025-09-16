fc7eeb492881b01ec54504a89ed6ac6b7a2fe609
MatiRojass <matiasnicolasrojas05@gmail.com>
2025-09-16 17:39:38 -0300

    inicializacion del programa

---
+++ src/empresatransporte/presentacion/Principal.java
@@ -0,0 +1,14 @@
+
+package empresatransporte.presentacion;
+
+import empresatransporte.MenuPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciar();
+    }
+
+}
